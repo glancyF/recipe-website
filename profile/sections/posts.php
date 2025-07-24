7bbbc7c4055b5a3b510d4af712e18391cecc0897
glancy <valentindesel@gmail.com>
2025-07-24 23:04:48 +0200

    Update post-style.css, posts.php

--- profile/sections/posts.php
+++ profile/sections/posts.php
@@ -1,14 +1,11 @@
 <?php
-
-
-require_once __DIR__ . '/../../profile/posts/post.php';
-
 ?>
 
-<h2>My Recipes</h2>
-<div id="recipesContainer" class="recipe-grid"></div>
-<div id="pagination" class="pagination-controls"></div>
-
+<div class="content-area">
+    <h2>My Recipes</h2>
+    <div id="recipesContainer" class="recipe-grid"></div>
+    <div id="pagination" class="pagination-controls fixed-bottom"></div>
+</div>
 <script src="/profile/posts/post.js"></script>
 
 
