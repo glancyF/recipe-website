cffe39dae4c727bc7add7a76a39c6da1d6b6bad7
glancy <valentindesel@gmail.com>
2025-08-04 15:25:44 +0200

    Fix post.js

--- profile/posts/post.js
+++ profile/posts/post.js
@@ -1,3 +1,5 @@
+import {LikeClicker} from "../../likes/likeClicker.js";
+
 document.addEventListener('DOMContentLoaded',()=>{
    const container = document.getElementById('recipesContainer');
    const pagination = document.getElementById('pagination');
@@ -104,23 +106,3 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 });
 
-export function LikeClicker(container){
-    container.addEventListener('click',async (e) =>{
-        if (e.target.classList.contains('fa-heart')) {
-            const likeIcon = e.target;
-            const card = likeIcon.closest('.like-container');
-            const recipeId = card.dataset.id;
-            const res = await fetch(`/likes/toggle_like.php?id=${recipeId}`,{
-                method: 'POST',
-                credentials: 'include'
-            });
-            const result = await res.json();
-            if(result.status ==='success') {
-                likeIcon.classList.toggle('liked',result.liked);
-                card.querySelector('.like-count').textContent = result.like_count;
-            } else{
-                alert('Failed to like the recipe');
-            }
-        }
-    });
-}
