d4bcb9c5d2860e8a7dd6bc337b7117cfe2a3d8c4
glancy <valentindesel@gmail.com>
2025-08-05 19:10:35 +0200

    Update favourites-styles.css and favourites.php

--- profile/sections/favourites.php
+++ profile/sections/favourites.php
@@ -1,7 +1,6 @@
-<section class="favourites-section">
+<section class="content-area">
     <h2>Favourites</h2>
     <div id="likedRecipes" class="recipes-grid"></div>
-    <div id="likedPagination" class="pagination"></div>
+    <div id="likedPagination" class="pagination-controls fixed-bottom"></div>
 </section>
-
 <script src="/profile/favourites/favourites.js" type="module"></script>
