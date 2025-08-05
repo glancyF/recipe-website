51ee62989c792d898ee150f19cb140ef28bda229
glancy <valentindesel@gmail.com>
2025-08-05 17:23:31 +0200

    Update mostLikedRecipe.js and mostLikedRecipe-styles.css

--- profile/overview/mostLikedRecipe.js
+++ profile/overview/mostLikedRecipe.js
@@ -4,6 +4,23 @@ document.addEventListener('DOMContentLoaded',()=>{
    const topRecipe = document.querySelector('.top-liked-recipe');
    if(topRecipe){
        LikeClicker(topRecipe);
+   topRecipe.addEventListener('click', async(e) =>{
+       const {target} = e;
+       if(target.classList.contains('delete-icon')){
+           const id = target.dataset.id;
+           if(confirm("Are you sure you want to delete this recipe?")){
+               const res = await fetch(`/profile/posts/delete.php?id=${id}`, {
+                   method: 'DELETE',
+                   credentials: 'include'
+               });
+               const result = await res.json();
+               if(result.status ==='success') {
+                   topRecipe.remove();
+               }else{
+                   alert('Failed to delete recipe')
+               }
+           }
+       }
+   });
    }
-
 });
