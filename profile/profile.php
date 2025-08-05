2c57f59cf698279c4fbc4e8fbce988ea9505aa7a
glancy <valentindesel@gmail.com>
2025-08-05 18:54:19 +0200

    Update profile.php

--- profile/profile.php
+++ profile/profile.php
@@ -8,6 +8,7 @@ $extra_css = '
     <link rel="stylesheet" href="/profile/password_change/password_change-styles.css">
     <link rel="stylesheet" href="/profile/posts/post-style.css">
     <link rel="stylesheet" href="/profile/overview/mostLikedRecipe-styles.css">
+    <link rel="stylesheet" href="/profile/favourites/favourites-styles.css">
 ';
 session_start();
 $section = $_GET['section'] ?? 'overview';
@@ -19,6 +20,7 @@ include "../includes/header.php";
             <ul class="menu">
                 <li><a href="?section=overview" class="<?= $section === 'overview' ? 'active' : '' ?>">Overview</a></li>
                 <li><a href="?section=posts" class="<?= $section === 'posts' ? 'active' : '' ?>">My recipes</a></li>
+                <li><a href="?section=favourites" class="<?= $section === 'favourites' ? 'active' : '' ?>">Favourites</a></li>
                 <li><a href="?section=settings" class="<?= $section === 'settings' ? 'active' : '' ?>">Settings</a></li>
                 <li><a href="?section=change_pass" class="<?= $section === 'change_pass' ? 'active' : '' ?>">Change password</a></li>
                 <li><a href="../profile/logout.php">Logout</a></li>
