69c001214e81455e2795b1698f24073c91a02b39
Valentyn Deshel <153648477+glancyF@users.noreply.github.com>
2025-08-16 21:04:30 +0200

    Create documentation.php

---
+++ documentation/documentation.php
@@ -0,0 +1,34 @@
+<?php
+$PageTitle = 'FAQ';
+$extra_css = '<link rel="stylesheet" href="/documentation/documentation-style.css">';
+include __DIR__ .'/../includes/header.php';
+?>
+
+<div class="container">
+    <section class="description-container">
+        <h1>Documentation</h1>
+        <section>
+            <h2>EN</h2>
+            <ul>
+            <!--<li><a href="...">Generated documentation from source code</a></li>-->
+                <li><a href="https://docs.google.com/document/d/1prsc0hnSlIycCFOVc1AbNZVUmBsIZX9frm-Ukx6Z0qQ/edit?usp=sharing">Task`s description</a></li>
+                <li><a href="https://docs.google.com/document/d/1_jeP4zhoBW6O9UIA9JZR2OE7CPpn1-p3nOs_i3LPPeM/edit?usp=sharing">Description of website functionality</a></li>
+                <li><a href="https://docs.google.com/document/d/1j-GrauMi8Y0NF28hyOKw9uRMpvu_9lmNMoBB1WxByq4/edit?usp=sharing">implementation description</a></li>
+            </ul>
+        </section>
+        <section>
+            <h2>CZ</h2>
+            <ul>
+                <!--<li><a href="...">Generated documentation from source code</a></li>-->
+                <li><a href="https://docs.google.com/document/d/1QKF47lpo8zzoWSBlIYq9t5UzbUjle-XtJ0yJFCEdaFI/edit?usp=sharing">Popis úlohy</a></li>
+                <li><a href="https://docs.google.com/document/d/15PV_gVLERgYoRA4isBscwg81BBNws1-eLlf7X_GNX0c/edit?usp=sharing">Uživatelská příručka</a></li>
+                <li><a href="https://docs.google.com/document/d/1JyUryvoIP08bICu7Wbifb4BFwWYaZVZdrQRK0eBwjk0/edit?usp=sharing">Popis implementace</a></li>
+            </ul>
+        </section>
+    </section>
+</div>
+
+
+<?php
+include __DIR__ .'/../includes/footer.php';
+?>
